030b66653f436e7a3133e6fc0c580b0f79a5b71e
yutoTachibana <yuto0324t@gmail.com>
2016-12-30 18:52:29 +0900

    first commit

---
+++ FuckapiForHardware/RECOMMEND.md
@@ -0,0 +1,13 @@
+# 推奨ハードウェア
+Braveridge AP 開発キット（BVMCN5103-AP KIT）
+AP-3X
+
+# 購入先：AMAZON
+https://www.amazon.co.jp/Braveridge-BVMCN5103-AP-KIT-AP-開発キット/dp/B00KH34KQ0/ref=sr_1_20?ie=UTF8&qid=1483182977&sr=8-20&keywords=ap-kit
+
+# 構成
+## BLEモジュール
+Braveridge : BVMCN5103-CFAC-BK
+
+## 加速度センサIC
+ANALOG DEVICES : ADXL362
---
+++ FuckapiForUnity/Assets/Fuckapi/FuckapiObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuckapiObject : MonoBehaviour {
+    public Transform startTransform, endTransform;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Transform getStart()
+    {
+        return startTransform;
+    }
+
+    public Transform getEnd()
+    {
+        return endTransform;
+    }
+
+    public float getSpeed()
+    {
+        return speed;
+    }
+}
